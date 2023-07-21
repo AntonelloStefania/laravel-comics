1984c6444e48db4e0e2073556cdddd50447313ba
AntonelloStefania <antonelloste94@gmail.com>
2023-07-21 13:58:29 +0200

    fatto header, iniziato footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,12 +10,39 @@
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
     <!-- Styles -->
     @vite('resources/js/app.js')
 
 </head>
 
+<style>
+    footer{
+        background-color:black
+    }
+</style>
+
 <body>
+    
+   @include('partials.header')
+    <div class="container">
+
+        <main></main>
+    </div>
+    <footer>
+        <div class="row">
+            <div class="col-12">
+                <ul class="list-unstyled d-flex align-items-center h-100 justify-content-around">
+                    <a href="/"><li><img class="w-50" src="{{Vite::asset('./resources/images/buy-comics-digital-comics.png')}}" alt=""> <span></span></li></a>
+                    <a href="/"><li><img class="w-50" src="{{Vite::asset('./resources/images/buy-comics-merchandise.png')}}" alt=""> <span></span></li></a>
+                    <a href="/"><li><img class="w-50" src="{{Vite::asset('./resources/images/buy-comics-shop-locator.png')}}" alt=""> <span></span></li></a>
+                    <a href="/"><li><img class="w-50" src="{{Vite::asset('./resources/images/buy-comics-subscriptions.png')}}" alt=""> <span></span></li></a>
+                    <a href="/"><li class="text-center"><img class="w-50" src="{{Vite::asset('./resources/images/buy-dc-power-visa.png')}}" alt=""> <span></span></li></a>
+                </ul>
+            </div>
+        </div>
+    </footer>
 
   
 </body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<header>
+        <div class="row">
+            <div class="col-12  text-end">
+                <a href="/"><span>DC POWER VISA</span></a><span></span>
+                <a href="/"><span>ADDITIONAL DC SITES</span></a><span></span>
+            </div>
+            <div class="col-12 d-flex justify-content-between">
+                <div>
+                    <img src="{{Vite::asset('./resources/images/dc-logo.png')}}" alt="logo">
+                </div>
+                <div>
+                    <ul class="list-unstyled d-flex ms-5">
+                        <a href="/"><li class="mx-2">CHARACTERS</li></a>
+                        <a href="/"><li class="mx-2">COMICS</li></a>
+                        <a href="/"><li class="mx-2">MOVIES</li></a>
+                        <a href="/"><li class="mx-2">TV</li></a>
+                        <a href="/"><li class="mx-2">GAMES</li></a>
+                        <a href="/"><li class="mx-2">COLLECTIBLES</li></a>
+                        <a href="/"><li class="mx-2">VIDEOS</li></a>
+                        <a href="/"><li class="mx-2">FANS</li></a>
+                        <a href="/"><li class="mx-2">NEWS</li></a>
+                        <a href="/"><li class="mx-2">SHOP</li></a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
